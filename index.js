f900ea953033a9d690b7dbc641150429fea13d0d
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2017-10-17 19:49:45 -0400

    update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
 let categories;
-
-function getCategories(number){
-  return fetch(`http://jservice.io/api/categories?count=5`)
-}
-
-function getCategoriesThen(number){
-  return getCategories(number).then(function(response){
-    return response.json()
-  })
-}
-
-function getCategoriesThenInterpretResponse(number){
-  return getCategoriesThen(number).then(function(response){
-    categories = response
-  })
-}
