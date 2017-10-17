fd99bec553d3df37c61fa39ea9d868a59610b88d
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2017-10-17 19:42:15 -0400

    update Readme.md to provide instructions

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const returnFirstTwoDrivers = (drivers) => {
-  return drivers.slice(0, 2)
+let categories;
+
+function getCategories(number){
+  return fetch(`http://jservice.io/api/categories?count=5`)
 }
 
-const returnLastTwoDrivers = (drivers) => {
-  return drivers.slice(drivers.length - 2, drivers.length)
+function getCategoriesThen(number){
+  return getCategories(number).then(function(response){
+    return response.json()
+  })
 }
 
-let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
-
-function createFareMultiplier(multiplyValue) {
-  return function (value) {
-    return multiplyValue * value;
-  };
+function getCategoriesThenInterpretResponse(number){
+  return getCategoriesThen(number).then(function(response){
+    categories = response
+  })
 }
-
-function selectDifferentDrivers(drivers, whichDrivers){
-  return whichDrivers(drivers)
-}
-
-const fareDoubler = createFareMultiplier(2);
-const fareTripler = createFareMultiplier(3);
